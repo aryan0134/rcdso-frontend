fc8234803865ed1434ce819dca6f7fb9a79d9f8a
Aryan Sindhu <81367645+aryan0134@users.noreply.github.com>
2023-06-24 13:38:27 +0530

    Update CommentForm.js

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { useAuthContext } from '../hooks/useAuthContext'
 import { AiOutlineSend } from 'react-icons/ai'
+import {useNavigate} from 'react-router-dom'
 
 function CommentForm({ addComment }) {
   const [text, setText] = useState('');
   const { user } = useAuthContext()
+  let navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(!user){
+      navigate('/login')
+    }
     addComment(text, `${user.user.firstName} ${user.user.lastName}`);
     setText('');
   };
